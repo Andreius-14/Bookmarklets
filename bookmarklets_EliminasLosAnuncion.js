376bdf0f44fc882cf9fb92e819669a2413c92f14
Andreius_14 <Andreius_14@outlook.com>
2024-05-12 18:33:01 -0500

    Script - Elimina Div que Contiene Anuncios

---
+++ bookmarklets_EliminasLosAnuncion.js
@@ -0,0 +1,42 @@
+// Función para seleccionar divs con etiquetas específicas
+function filterHtml (etiquetasArray) {
+  // Convertir las etiquetas a un array
+
+  const divsSeleccionados = []
+  for (const etiqueta of etiquetasArray) {
+    const divs = document.querySelectorAll(`[id*="${etiqueta}"],[class*="${etiqueta}"]`)
+    console.log(etiqueta)
+    console.log(divs.length)
+    divsSeleccionados.push(...Array.from(divs)) // Convertir NodeList a array
+  }
+
+  return divsSeleccionados
+}
+
+// Función para editar los divs seleccionados
+function edit (divs) {
+  divs.forEach(element => {
+    element.remove()
+  })
+}
+
+const etiquetasAnuncios = [
+  // 'ad',
+  'ads',
+  'ad-', // Seguido de un guión y otro texto
+  'adframe',
+  'sponsored',
+  'google_ads_iframe',
+  'adsense',
+  'adx',
+  'googletag_div',
+  'div-gpt',
+  'fb-ad',
+  'fb-pixel',
+  'amazon-ad',
+  'aax_dp'
+]
+const listaFiltrada = filterHtml(etiquetasAnuncios)
+
+console.log('Divs seleccionados:', listaFiltrada)
+edit(listaFiltrada)
